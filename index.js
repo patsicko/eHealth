80f624fba987fd2a87fc3202610e3e3932683cfa
patsicko <patsicko@g>
2022-08-03 19:42:48 +0200

    Feat:React project

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ console.log(password);
 const header={"Accept":"application/json"};
 
 const formData=new FormData();
-// formData.append("phone",phone.value);
-// formData.append("password",password.value);
+formData.append("phone",phone.value);
+formData.append("password",password.value);
 
-formData.append("phone","0785189326");
+// formData.append("phone","0785189326");
 
-formData.append("password","12345678");
+// formData.append("password","12345678");
 // const message=JSON.stringify({"phone":phone.value,"password":password.value});
 
 const log=async()=>{
@@ -39,8 +39,7 @@ login.addEventListener("click",e=>{
    e.preventDefault();
 loginField.classList.toggle("show-login");
 
-//    main.classList.toggle("hide-main");
-//    dashboard.classList.toggle("show-dashboard");
+
 
  
 
@@ -50,6 +49,8 @@ loginField.classList.toggle("show-login");
 loginButton.addEventListener("click",e=>{
     e.preventDefault();
     log();
+       main.classList.toggle("hide-main");
+       dashboard.classList.toggle("show-dashboard");
 })
 
 localStorage.setItem("token","eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiNzRjMGRmYzQ0YTY2YzYyZDU4ZDBmNDJlMmQ3NWYxYWVkMGNkMjlhZDBmMjMxODhiY2RlMTZmNjBiYjBjMmI5OGQ0YjQ0ZjdkMmM1MzYyMWEiLCJpYXQiOjE2NTkzODA3MDMsIm5iZiI6MTY1OTM4MDcwMywiZXhwIjoxNjkwOTE2NzAzLCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.dkxiUSGUeIGVRziGDW3URSXwMVjQdkV6FM3kf5YwGRcGyKOkAwyiEa3_hAs7nWx-Dm1e0votwJgfgTxrghXwAPPOHSUf_cgr-lCz65XRPhagK7cXvaDJRGaKcHCM4hhpYvs05gB0vWlBP3k1ob4WAkOT0h4jp26kq0JQ_VysMMDtPuqheOWX_eyM6pilsE1ye3kciPYeNiSaku3Y55IDz78D7HZv0BEiOj_RFqvQzeTQ-kKrOkoLsGIBwl-cOVxffhAyDyRq9Hy_QXIevqYB_Dy8GYp4Iz_kYG-4TFxm1CETuxcfOtgi5cCuJp_enS6tfdmZCln-GQ40iV7-lgwrlO9QcrfTnOMsyCm_4jW3KoCMbtGv63WIyOmqBvjLvkKcz4RTeVXova3_d-rOW48nE4ecLO3CHWG-J6AFdMx62vAlhIg-CxKPdq8jgjHBIysY29eVFFbvKV3v44oDz27gVpgbWbR5-5jlmTc0Ro1YjUW9e4O6HT0dHnlJfi2YteJ5gwy_QSDlRG1XXNRZj8O13oNs3DIETCbsH3qDT46apsV7a-vgZ-yYJ-UhKUVbmatlPCte2wR9VC5-bS71QTiu1zOCCextyLAIPXAlLEmbaN7H-GGuAyivnSTWVSRBi97733dCHMZ6oRthek7ZV82JChxMvBiZ_41xelC74fhN454")
@@ -67,3 +68,5 @@ localStorage.setItem("token","eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxI
 //     createHospital();
 
 // })
+
+
